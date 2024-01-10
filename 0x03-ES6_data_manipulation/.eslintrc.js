9ba4fed75a2aad47d6dd6e0b64fd56ef5a539848
Netrobe Ubuntu <netrobepy@gmail.com>
2024-01-10 19:11:32 +0100

    update

--- 0x03-ES6_data_manipulation/.eslintrc.js
+++ 0x03-ES6_data_manipulation/.eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
       'LabeledStatement',
       'WithStatement',
     ],
+    'quotes': ['error', 'double'],
   },
   overrides:[
     {
--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,14 +1,11 @@
-
 /**
- * Return an array of students
- * @returns {Array} Array of students
+ * Retrieves a list of students.
+ * @returns {{id: Number, firstName: String, location: String}[]}
  */
-function getListStudents() {
+export default function getListStudents() {
   return [
-    { id: 1, firstName: "Guillaume", location: "San Francisco" },
-    { id: 2, firstName: "James", location: "Columbia" },
-    { id: 3, firstName: "Serena", location: "San Francisco" },
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
-
-export default getListStudents;
