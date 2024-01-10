1015962d8f1357098ff91a18de18f5aeb132655e
Netrobe Ubuntu <netrobepy@gmail.com>
2024-01-10 19:16:56 +0100

    update

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Array} students
+ * @returns {Array} Array of student ids
+ */
+function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.map((student) => student.id);
+}
+
+export default getListStudents;
