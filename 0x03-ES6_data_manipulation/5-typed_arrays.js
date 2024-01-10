24ecebfc43ce324180e1e2c25f21649775edfafa
Netrobe Ubuntu <netrobepy@gmail.com>
2024-01-11 00:24:18 +0100

    update

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,6 +3,9 @@
  * @returns {Array} Array of student ids
  */
 function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
   const buffer = new ArrayBuffer(length);
   const view = new Int8Array(buffer);
   view[position] = value;
