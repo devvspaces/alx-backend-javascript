5ec3141d16fa7a110a4376781a240f23613cfdd1
Netrobe Ubuntu <netrobepy@gmail.com>
2024-01-10 23:31:52 +0100

    update

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,14 +3,16 @@
  * @returns {Array} Array of student ids
  */
 function updateStudentGradeByCity(students, city, newGrades) {
-  const result = students.filter((student) => student.location === city).map(student => {
-    const grade = newGrades.filter(obj => obj.studentId == student.id)[0];
-    return {
-      ...student,
-      grade: grade ? grade.grade : 'N/A'
-    }
-  })
-  return result
+  const result = students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const grade = newGrades.filter((obj) => obj.studentId == student.id)[0];
+      return {
+        ...student,
+        grade: grade ? grade.grade : 'N/A',
+      };
+    });
+  return result;
 }
 
 export default updateStudentGradeByCity;
