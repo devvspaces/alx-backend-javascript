9921c8218f4e86fb41701cace36457eba7ff9aa9
Netrobe Ubuntu <netrobepy@gmail.com>
2024-01-11 09:38:29 +0100

    update

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,13 +2,13 @@
  * @param {Array} students
  * @returns {Array} Array of student ids
  */
-function cleanSet(set, startstring) {
+function cleanSet(set, startString) {
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
   return Array.from(set)
-    .filter((str) => str.startsWith(startstring))
-    .map((str) => str.substring(startstring.length))
+    .filter((str) => str.startsWith(startString))
+    .map((str) => str.substring(startString.length))
     .join('-');
 }
 
