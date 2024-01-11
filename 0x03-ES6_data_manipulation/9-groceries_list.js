ce53001a8089c5b927071ce3609d7fd4afe9d769
Netrobe Ubuntu <netrobepy@gmail.com>
2024-01-11 09:41:18 +0100

    update

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Array} students
+ * @returns {Array} Array of student ids
+ */
+function groceriesList() {
+  const groceries = new Map();
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 10);
+  groceries.set('Pasta', 1);
+  groceries.set('Rice', 1);
+  groceries.set('Rice', 5);
+  return groceries;
+}
+
+export default groceriesList;
