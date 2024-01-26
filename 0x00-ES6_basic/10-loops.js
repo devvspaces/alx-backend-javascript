d57a54889e5eaee45cba40d19f5bab303802c4ca
Netrobe Ubuntu <netrobepy@gmail.com>
2024-01-26 10:51:54 +0100

    update

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   let idx = 0;
-  for (let value of array) {
+  for (const value of array) {
     array[idx] = appendString + value;
-    idx++;
+    idx += 1;
   }
 
   return array;
