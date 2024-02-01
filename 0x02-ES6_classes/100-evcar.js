d51780233ae169e046fa9df5fd164fa87cad6472
Netrobe Ubuntu <netrobepy@gmail.com>
2024-02-01 08:47:13 +0100

    update

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,13 @@
 import Car from './10-car';
 
+Car.prototype.cloneCar = function cloneCar() {
+  const newCar = new Car();
+  return newCar;
+};
+
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
-  }
-
-  cloneCar() {
-    const newCar = new Car();
-    return newCar;
   }
 }
