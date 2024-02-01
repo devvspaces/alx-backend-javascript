e50ced8231326fc5274d69d6966c1173fbdcbd5f
Netrobe Ubuntu <netrobepy@gmail.com>
2024-02-01 06:32:29 +0100

    update

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -23,14 +23,17 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
+    if (typeof newName !== 'string') throw TypeError('Name must be a string');
     this._name = newName;
   }
 
   set length(newLength) {
+    if (typeof newLength !== 'number') throw TypeError('Length must be a number');
     this._length = newLength;
   }
 
   set students(newStudents) {
+    if (!Array.isArray(newStudents)) throw TypeError('Students must be an array of strings');
     this._students = newStudents;
   }
 }
