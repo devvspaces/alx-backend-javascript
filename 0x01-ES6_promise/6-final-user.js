d12a1995e1fb0d1e5a985f7d0c8c4c9e3b660075
Netrobe Ubuntu <netrobepy@gmail.com>
2024-02-01 06:07:47 +0100

    update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,19 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     uploadPhoto(fileName),
     signUpUser(firstName, lastName),
-  ]);
+  ]).then((value) => {
+    const photo = value[0];
+    const user = value[1];
+    const photoStatus = photo.status;
+    const userStatus = user.status;
+    if (photoStatus === 'rejected') {
+      photo.value = photo.reason;
+      delete photo.reason;
+    }
+    if (userStatus === 'rejected') {
+      user.value = `Error: ${user.reason.message}`;
+      delete user.reason;
+    }
+    return [photo, user];
+  });
 }
